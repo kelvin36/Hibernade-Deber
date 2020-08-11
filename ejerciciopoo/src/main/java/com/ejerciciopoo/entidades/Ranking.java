a7c5d194b7cc5ec6c885c5384bee892a19b41a23
crist <crist@LAPTOP-5QRD4L1D>
2020-08-11 12:45:36 -0500

    ejercicio en clase

---
+++ ejerciciopoo/src/main/java/com/ejerciciopoo/entidades/Ranking.java
@@ -0,0 +1,47 @@
+package com.ejerciciopoo.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Ranking {
+	@Id
+	@GeneratedValue
+	private int id;
+	private Integer ranking;
+	
+
+	public Ranking() {
+	}
+	
+
+	public Ranking(Integer ranking) {
+		super();
+		this.ranking = ranking;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getRanking() {
+		return ranking;
+	}
+
+	public void setRanking(Integer ranking) {
+		this.ranking = ranking;
+	}
+
+
+	
+	
+	
+	
+
+}
